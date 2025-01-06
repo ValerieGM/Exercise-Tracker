711658c152e297bd2a6bfa2c5d7d5a01b02eef37
ValerieGM <mhizhagongora@gmail.com>
2025-01-06 06:26:13 +0000

    add: exercise and log endpoint

--- index.js
+++ index.js
@@ -26,8 +26,17 @@ const Users = mongoose.model('Users', new mongoose.Schema({
   username: String
 }));
 
+const Log = mongoose.model('Log', new mongoose.Schema({
+  userid: String,
+  username: String,
+  description: String,
+  duration: Number,
+  date: { type: Date, default: Date.now }
+}));
+
 const Exercises = mongoose.model('Exercises', new mongoose.Schema({
   id: String,
+  username: String,
   description: { type: String, required: true },
   duration: { type: Number, required: true },
   date: Date
@@ -40,11 +49,11 @@ app.post('/api/users', async (req, res) => {
     const user = new Users({
       username: username
     });
-    
+
     user.save();
 
     res.status(200).json({
-      username: user.username, 
+      username: user.username,
       _id: user._id
     });
   } catch (err) {
@@ -60,6 +69,84 @@ app.get('/api/users', async (req, res) => {
     console.log(users);
 
     res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  try {
+    const userId = req.params;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = req.body.date
+      ? new Date(req.body.date)
+      : new Date();
+
+    const user = await Users.findById(userId);
+
+    if (user === null) {
+      res.end("User Not Found");
+    }
+
+    const log = new Exercises({
+      id: userId,
+      username: user.username,
+      description: description,
+      duration: duration,
+      date: date
+    });
+
+    console.log(log)
+
+    let logDate = log.date.toDateString();
+    console.log(log)
+
+    await log.save();
+
+    res.status(200).json({
+      id: userId,
+      username: log.username,
+      description: log.description,
+      duration: log.duration,
+      date: logDate
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get('/api/users/:_id/logs', async (req, res) => {
+  try {
+    const user = await Users.findById(req.params);
+    const limit = Number(req.query.limit) || 0;
+    
+    const to = req.query.to || new Date(Date.now());
+    const from = req.query.from || new Date(0);
+
+    const log = await Log.find({
+      userid: req.params.id,
+      date: { $gte: from , $lte: to }
+    })
+    .select("-_id -userid -__v")
+    .limit(limit);
+    
+    let userLog = log.map((each) => {
+      return {
+        description: each.description,
+        duration: each.duration,
+        date: new Date(each.date).toDateString(),
+      };
+    });
+
+    res.json({
+      _id: req.params,
+      username: user.username,
+      count: log.length,
+      log: userLog,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: err.message });
